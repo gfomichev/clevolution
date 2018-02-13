a307f69787d7a65fcb088b07c078109b5d5d842b
gleb <gleb767@gmail.com>
2018-02-13 09:14:46 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
+//Object iteration, thanks to github.com/crossjs
+Object.prototype.forEach = function(fn) {
+  var object = this, returned;
+
+  Object.keys(object).forEach(function(key) {
+    if (returned === false) {
+      return;
+    }
+
+    returned = fn.call(null, object[key], key, object);
+  });
+};
+
 function getByElement(array = Heroes, element) {
     var b = {};
     Object.keys(array).filter(hell => array[hell].element == element.toLowerCase()).forEach(el => {
